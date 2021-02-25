7b7d9ea79ec411a57536f0f79280f3f6c749c98d
noheropage <mursepage@gmail.com>
2021-02-24 19:13:34 -0800

    Modify init function in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = () =>
@@ -58,11 +59,17 @@ const questions = () =>
     ]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const readme = generateMarkdown();
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    questions();
+    questions().then((inputs) => {
+        try {
+            const readMe = makeReadMe(inputs);
+        }
+    });
 }
 
 // Function call to initialize app
